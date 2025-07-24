5d05ebe4e2d890f892f7bdefd528ffe572fcaf3b
AlecCross <cross2472@gmail.com>
2025-07-24 15:04:39 +0000

    New try configurate offline working

--- app/sw.ts
+++ app/sw.ts
@@ -1,25 +1,25 @@
 import { defaultCache } from "@serwist/next/worker";
 import type { PrecacheEntry, SerwistGlobalConfig } from "serwist";
-import { Serwist } from "serwist";
+import { Serwist, CacheFirst, NetworkFirst, CacheableResponsePlugin, ExpirationPlugin } from "serwist";
 
 declare global {
-    interface WorkerGlobalScope extends SerwistGlobalConfig {
-    // Change this attribute's name to your `injectionPoint`.
-    // `injectionPoint` is an InjectManifest option.
-    // See https://serwist.pages.dev/docs/build/configuring
-
-          __SW_MANIFEST: (PrecacheEntry | string)[] | undefined
-     }
+  interface WorkerGlobalScope extends SerwistGlobalConfig {
+    __SW_MANIFEST: (PrecacheEntry | string)[] | undefined;
+  }
 }
 
 declare const self: ServiceWorkerGlobalScope;
+
+const appPaths = [
+  '/',
+];
 
 const serwist = new Serwist({
   precacheEntries: self.__SW_MANIFEST,
   precacheOptions: {
     cleanupOutdatedCaches: true,
     concurrency: 10,
-    ignoreURLParametersMatching: [/.*/],
+    ignoreURLParametersMatching: [],
   },
   skipWaiting: true,
   clientsClaim: true,
@@ -35,7 +35,34 @@ const serwist = new Serwist({
       },
     ],
   },
-  runtimeCaching: defaultCache,
+  runtimeCaching: [
+  {
+    matcher: ({ url, request }: any) => url.pathname.startsWith('/apps-icons/') && request.destination === 'image',
+    handler: new CacheFirst({
+      cacheName: 'apps-icons-cache',
+      plugins: [
+        new CacheableResponsePlugin({ statuses: [0, 200] }),
+        new ExpirationPlugin({ maxEntries: 100, maxAgeSeconds: 30 * 24 * 60 * 60 }),
+      ],
+    }),
+  },
+  {
+    matcher: ({ url, request }: any) =>
+        request.destination === 'document' &&
+        appPaths.some(path =>
+          path === '/'
+            ? url.pathname === '/'
+            : url.pathname.startsWith(path)
+        ),
+    handler: new NetworkFirst({
+      cacheName: 'pwa-app-pages-cache',
+      plugins: [
+        new CacheableResponsePlugin({ statuses: [0, 200] }),
+        new ExpirationPlugin({ maxEntries: 20, maxAgeSeconds: 7 * 24 * 60 * 60 }),
+      ],
+    }),
+  },
+],
 });
 
-serwist.addEventListeners();
+serwist.addEventListeners(); // Обов'язково в кінці, без додаткових кастомних слухачів 'install'
